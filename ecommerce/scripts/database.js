b13a3eb3f91326e5f2144303c448559178738e3f
DeveloperNico <nickboytube0418@gmail.com>
2026-04-20 11:09:33 -0300

    style: foi retirado elementos desnecessários - card de tipo do livro - card com o tipo da entrega

--- ecommerce/scripts/database.js
+++ ecommerce/scripts/database.js
@@ -4,6 +4,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"A seleção", 
         preco: 27.00, 
         descricao:'Em "A Seleção", trinta e cinco garotas competem para se tornar a futura esposa do príncipe Maxon e, eventualmente, a rainha. America Singer, no entanto, vê essa oportunidade como um pesadelo, pois precisa abandonar o rapaz que ama, sua família e seu lar, entrando em uma competição por algo que não deseja. Ao conhecer o príncipe, ela começa a questionar suas escolhas, percebendo que a vida que sempre sonhou pode ser menos atrativa do que o futuro que nunca imaginou.',
+        autora: "Colleen Hoover",
+        autoraSobre: "KIERA CASS nasceu em 1981, na Carolina do Sul, Estados Unidos. Formou-se em história na Universidade de Radford, na Virginia, e hoje mora em Christiansburg, com o marido e os dois filhos. É autora da série A Seleção, que já vendeu mais de 2 milhões de exemplares no Brasil, e de A sereia. Odeia motoristas que não dão seta, viajar de avião e salada; ama materiais de papelaria, boy bands e sobremesa.",
         imagemProduto: {
             img1:"./images/livros/A_selecao/img1.png",
             img2:"./images/livros/A_selecao/img2.png",
@@ -35,6 +37,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"A escolha", 
         preco: 26.00, 
         descricao:"America, uma candidata improvável da Seleção, entrou na competição para se afastar de Aspen, o garoto que a magoou. Porém, ao conhecer o príncipe Maxon, uma amizade se transformou em algo mais. Apesar disso, obstáculos como o retorno de Aspen ao palácio como guarda e o desdém do rei por America dificultam seu relacionamento com Maxon. Enquanto lida com rivalidades e tenta ganhar apoio político, America começa a questionar o sistema de castas e a abordagem contra os ataques rebeldes, colocando tudo em risco.",
+        autora: "Colleen Hoover",
+        autoraSobre: "KIERA CASS nasceu em 1981, na Carolina do Sul, Estados Unidos. Formou-se em história na Universidade de Radford, na Virginia, e hoje mora em Christiansburg, com o marido e os dois filhos. É autora da série A Seleção, que já vendeu mais de 2 milhões de exemplares no Brasil, e de A sereia. Odeia motoristas que não dão seta, viajar de avião e salada; ama materiais de papelaria, boy bands e sobremesa.",
         imagemProduto: {
             img1:"./images/livros/A_escolha/img1.png",
             img2:"./images/livros/A_escolha/img2.png",
@@ -49,6 +53,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"A coroa", 
         preco: 25.00, 
         descricao:"Em A Herdeira, Eadlyn, filha de America e Maxon, enfrenta sua própria Seleção. Inicialmente cética sobre o amor, ela começa a se abrir aos candidatos, que se tornam seu apoio em meio a crescentes responsabilidades governamentais e a rejeição pública. Enquanto lida com a pressão de escolher um marido, um candidato especial começa a ganhar seu coração.",
+        autora: "Colleen Hoover",
+        autoraSobre: "KIERA CASS nasceu em 1981, na Carolina do Sul, Estados Unidos. Formou-se em história na Universidade de Radford, na Virginia, e hoje mora em Christiansburg, com o marido e os dois filhos. É autora da série A Seleção, que já vendeu mais de 2 milhões de exemplares no Brasil, e de A sereia. Odeia motoristas que não dão seta, viajar de avião e salada; ama materiais de papelaria, boy bands e sobremesa.",
         imagemProduto: {
             img1:"./images/livros/A_coroa/img1.jpg",
             img2:"./images/livros/A_coroa/img2.jpg",
@@ -63,6 +69,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"A herdeira", 
         preco: 24.00, 
         descricao:"Vinte anos atrás, America Singer participou da Seleção e conquistou o coração do príncipe Maxon. Agora chegou a vez da princesa Eadlyn, filha do casal. Prestes a conhecer os trinta e cinco pretendentes que irão disputar sua mão numa nova Seleção, ela não tem esperanças de viver um conto de fadas como o de seus pais… Mas assim que a competição começa, ela percebe que encontrar seu príncipe encantado talvez não seja tão impossível quanto parecia.",
+        autora: "Colleen Hoover",
+        autoraSobre: "KIERA CASS nasceu em 1981, na Carolina do Sul, Estados Unidos. Formou-se em história na Universidade de Radford, na Virginia, e hoje mora em Christiansburg, com o marido e os dois filhos. É autora da série A Seleção, que já vendeu mais de 2 milhões de exemplares no Brasil, e de A sereia. Odeia motoristas que não dão seta, viajar de avião e salada; ama materiais de papelaria, boy bands e sobremesa.",
         imagemProduto: {
             img1:"./images/livros/A_herdeira/img1.jpg",
             img2:"./images/livros/A_herdeira/img2.jpg",
@@ -77,6 +85,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Eu sempre morro", 
         preco: 26.00, 
         descricao:'É com muito orgulho que anunciamos que um dos principais hits da literatura independente brasileira, com mais de 25 mil livros vendidos em dois anos, conquistando até mesmo o posto de livro mais vendido na categoria de literatura nacional da Amazon Brasil (onde ficou no top 3 por todo o ano de 2023), o livro de prosa poética "eu sempre morro", acaba de ganhar uma reedição, feita com muito carinho pela editora Devir, poesia e prosa. A nova edição conta com 16 novas páginas, novos textos e ilustrações, livro impresso em papel pólen bold 90 e capa com acabamento em verniz aplicado.',
+        autora: "Kaio Bruno Dias",
+        autoraSobre: 'Kaio Bruno Dias, com uma linguagem simples e sensível, aborda temas como ausência, perda e solidão em suas obras. Ele publicou vários livros e teve dois textos incluídos na mostra "Poesia Agora" no Museu da Língua Portuguesa.',
         imagemProduto: {
             img1:"./images/livros/EuSempreMorro/img1.jpg",
             img2:"./images/livros/EuSempreMorro/img2.jpg",
@@ -92,6 +102,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Mindset", 
         preco: 30.00, 
         descricao:"Carol S. Dweck, ph.D., professora de psicologia na Universidade Stanford e especialista internacional em sucesso e motivação, desenvolveu, ao longo de décadas de pesquisa, um conceito fundamental: a atitude mental com que encaramos a vida, que ela chama de “mindset”, é crucial para o sucesso. Dweck revela de forma brilhante como o sucesso pode ser alcançado pela maneira como lidamos com nossos objetivos. O mindset não é um mero traço de personalidade, é a explicação de por que somos otimistas ou pessimistas, bem-sucedidos ou não. Ele define nossa relação com o trabalho e com as pessoas e a maneira como educamos nossos filhos. É um fator decisivo para que todo o nosso potencial seja explorado.",
+        autora: "Carol S. Dweck",
+        autoraSobre: 'Carol S. Dweck, Ph.D., é uma renomada pesquisadora em psicologia, lecionou na Universidade de Columbia e atualmente ensina em Stanford. Seu livro Self-Theories foi premiado. Ela mora em Palo Alto, Califórnia, com o marido.',
         imagemProduto: {
             img1:"./images/livros/Mindset/img1.jpg",
             img2:"./images/livros/Mindset/img2.jpg",
@@ -107,6 +119,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Will & Will. Um nome, um destino", 
         preco: 29.99, 
         descricao:"Em uma noite fria, numa improvável esquina de Chicago, Will Grayson encontra... Will Grayson. Os dois adolescentes dividem o mesmo nome e a dor do coração partido. Um Will é amigo do mais expansivo gay de sua escola. O outro precisa explicar à própria mãe sua orientação sexual. Até que Tiny, o melhor amigo gay do primeiro Will, acaba se tornando o possível amor do outro Will. Apesar das origens completamente diferentes, esses inesperados encontros fazem com que os meninos de mesmo nome estejam prestes a embarcar juntos em uma aventura de épicas proporções. Amor adolescente, intriga, raiva, sofrimento e amizade. Tudo isso temperado com doses maciças de comédia.",
+        autora: "John Green",
+        autoraSobre: 'John Green é um autor premiado de best-sellers como Quem é Você, Alasca? e A Culpa é das Estrelas. Ele recebeu diversos prêmios literários e foi considerado uma das 100 pessoas mais influentes do mundo pela revista TIME. Ele também co-criou os Vlogbrothers e a série educacional CrashCourse com seu irmão Hank.',
         imagemProduto: {
             img1:"./images/livros/Will&Will/img1.jpg",
             img2:"./images/livros/Will&Will/img2.jpg",
@@ -122,6 +136,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Tudo é rio", 
         preco: 32.00, 
         descricao:'O romance Tudo é Rio narra a história de Dalva e Venâncio, cuja vida é transformada por uma perda trágica causada pelo ciúme doentio de Venâncio, e Lucy, uma prostituta que forma um triângulo amoroso com o casal. Martha Medeiros elogia a obra como uma "obra-prima" que prende o leitor até a última página. A metáfora do rio é usada para descrever a narrativa fluida e intensa, abordando temas de suor, saliva, sangue, lágrimas e sêmen, com uma escrita habilidosa e poética.',
+        autora: "Carla Madeira",
+        autoraSobre: 'Carla Madeira é uma escritora brasileira, autora de romances como Tudo é Rio, A Natureza da Mordida e Véspera. Nascida em Belo Horizonte em 1964, também é jornalista e publicitária. Seu livro Tudo é Rio foi um grande sucesso de vendas. Além de escrever, Carla é diretora de criação na agência Lápis Raro.',
         imagemProduto: {
             img1:"./images/livros/TudoERio/img1.jpg",
             img2:"./images/livros/TudoERio/img2.jpg",
@@ -137,6 +153,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Talvez você precise conversar com alguém", 
         preco: 32.99, 
         descricao:"O livro de Lori Gottlieb combina histórias de sua carreira como terapeuta com sua própria experiência como paciente, mostrando que terapeutas também enfrentam desafios pessoais. Ela narra sua jornada de autodescoberta e destaca a universalidade das questões humanas e a capacidade de mudar. A obra é uma homenagem à natureza humana, ressaltando a importância de ser ouvido e de ouvir, e enfatiza a coragem necessária para a aventura do autoconhecimento.",
+        autora: "Carol Dweck",
+        autoraSobre: 'Carol S. Dweck, Ph.D., é uma renomada pesquisadora em personalidade, psicologia social e do desenvolvimento. Lecionou na Universidade de Columbia e atualmente é professora em Stanford. Seu livro Self-Theories foi premiado e seu trabalho é amplamente reconhecido. Ela reside em Palo Alto, Califórnia, com o marido.',
         imagemProduto: {
             img1:"./images/livros/TalvezVocePreciseConversarComAlguem/img1.jfif",
             img2:"./images/livros/TalvezVocePreciseConversarComAlguem/img2.jpg",
@@ -152,6 +170,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Aristoteles e Dantes mergulham nas águas do mundo", 
         preco: 29.99, 
         descricao:"Em Aristóteles e Dante descobrem os segredos do Universo, Ari e Dante enfrentam o último ano do ensino médio, abrindo-se para novas amizades e experiências. A vida de Ari é transformada pelo apoio de Dante, especialmente após uma perda devastadora. Nesta continuação, eles exploram um novo mundo cheio de desafios e possibilidades, aprofundando seus laços e descobrindo mais sobre si mesmos e suas capacidades de amar.",
+        autora: "Benjamin Alire",
+        autoraSobre: 'Benjamin Alire Sáenz é um renomado poeta, romancista e escritor de livros infantis, conhecido por abordar temas de cultura hispânica e LGBT. Seu livro Aristóteles e Dante Descobrem os Segredos do Universo recebeu prêmios importantes. Ele vive em El Paso, Texas.',
         imagemProduto: {
             img1:"./images/livros/AristotelesEDante2/img1.jpg",
             img2:"./images/livros/AristotelesEDante2/img2.jpg",
@@ -166,6 +186,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Aristóteles e Dante descobrem os segredos do Universo", 
         preco: 34.90, 
         descricao:"Em Os Segredos do Universo por Aristóteles e Dante, Aristóteles e Dante formam uma amizade transformadora durante um verão tedioso. Apesar de suas diferenças, eles se aproximam e, através dessa relação, descobrem mais sobre si mesmos e o tipo de pessoa que querem ser. Enquanto Dante é confiante e apaixonado por poesia, Ari é introspectivo e marcado pelo irmão na prisão. Juntos, eles compartilham livros, sonhos e risadas, descobrindo que a amizade e o amor podem revelar os segredos do universo.",
+        autora: "Benjamin Alire",
+        autoraSobre: 'Benjamin Alire Sáenz é um renomado poeta, romancista e escritor de livros infantis, conhecido por abordar temas de cultura hispânica e LGBT. Seu livro Aristóteles e Dante Descobrem os Segredos do Universo recebeu prêmios importantes. Ele vive em El Paso, Texas.',
         imagemProduto: {
             img1:"./images/livros/AristotelesEDante/img1.jpg",
             img2:"./images/livros/AristotelesEDante/img2.jpg",
@@ -180,6 +202,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Textos para tocar cicatrizes", 
         preco: 24.90, 
         descricao:"Em Textos para Tocar Cicatrizes, Igor Pires explora sentimentos nos relacionamentos, sociedade, luto e família. Ele destaca a importância de honrar nossas cicatrizes adquiridas através das experiências, pois são responsáveis pelas nossas histórias. Com sua sensível escrita, o autor reflete sobre a humanidade, enquanto as ilustrações de Anália Moraes complementam e intensificam as emoções descritas nos textos.",
+        autora: "Igor Pires",
+        autoraSobre: 'Igor Pires é um autor brasileiro conhecido pela série de livros Textos Cruéis Demais para Serem Lidos Rapidamente. Seus textos, que abordam temas como amor, perda e autoestima, ganharam popularidade nas redes sociais. Igor foi o autor nacional mais lido em 2020.',
         imagemProduto: {
             img2:"./images/livros/TextosParaTocarCicatrizes/img2.jpg",
             img1:"./images/livros/TextosParaTocarCicatrizes/img1.jpg",
@@ -194,6 +218,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Todas as flores que não te enviei", 
         preco: 27.90, 
         descricao:"Todas as flores que não te enviei trata de sentimentos, inclusive aqueles que lutamos para esconder, mas que estão ali, esperando o momento exato para serem libertos. A inspiração dos poemas de Felipe Rocha chega através da sua imaginação e da sensibilidade, e também da percepção de tudo que está ao seu redor. Foi depois de muita reflexão que, em 2016, nasceu o tipobilhete, como forma de trazer um pouco de alegria para o dia a dia e transbordar aquilo que atitudes não são capazes de demonstrar.",
+        autora: "Felipe Rocha",
+        autoraSobre: 'Felipe Rocha é um ator, autor e músico franco-brasileiro, nascido em Paris em 1972 e radicado no Brasil desde 1978. Ele é conhecido por seu trabalho no teatro e na televisão, além de ser vencedor do Prêmio Shell de teatro. Felipe também é membro do grupo de teatro Foguetes Maravilha e da banda Brasov.',
         imagemProduto: {
             img2:"./images/livros/TodasAsFloresQueNaoTeEnviei/img2.jpg",
             img1:"./images/livros/TodasAsFloresQueNaoTeEnviei/img1.jpg",
@@ -208,6 +234,8 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         tituloProduto:"Leia um café e tome uma poesia: antes que esfrie", 
         preco: 29.90, 
         descricao:"O livro “Leia um café e tome uma poesia”, do engenheiro e poeta Fagner Mera, é um convite ao leitor a uma série de reflexões, sobre temas do cotidiano. Abordando de maneira metafórica e poética, de forma simples, questões complexas como amor, desamor, felicidade, autocuidado, autoconhecimento e tantos outros sentimentos humanos, permitindo que o leitor pare por um momento, e experimente uma introspecção em sua própria realidade",
+        autora: "Fagner Mera",
+        autoraSobre: 'Fagner Mera é um professor e poeta brasileiro, autor de livros como Leia um café e tome uma poesia: antes que esfrie e Poesias que escrevi enquanto aprendia a viver. Seus poemas abordam temas cotidianos como amor, perda e autoconhecimento.',
         imagemProduto: {
             img2:"./images/livros/LeiaUmCafeETomeUmaPoesia/img2.jpg",
             img1:"./images/livros/LeiaUmCafeETomeUmaPoesia/img1.jpg",
